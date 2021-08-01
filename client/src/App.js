a41512c5629215fe3d17ed1262617c7d98022d31
khushipr <khushipr01@gmail.com>
2021-08-01 14:34:00 +0530

    React setup and structure building

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import ShowStudent from './components/showStudent/showStudent/js';
+import CreateStudent from './components/createStudent/createStudent.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hi there!!</h1>
-      </header>
+      <Container maxWidth='lg'>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Students Create & Show</Typography>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              {/* Table */}
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <ShowStudent />
+                </AppBar>
+              </Grid>
+              {/* Form */}
+              <Grid item xs={12} sm={4}>
+              <AppBar className={classes.appBar} position="static" color="inherit">
+                  <CreateStudent />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }

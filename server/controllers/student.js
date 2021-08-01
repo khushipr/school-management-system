f209d4bf8209cfc6dc03aa6f435aa487c6094d71
khushipr <khushipr01@gmail.com>
2021-08-01 13:48:22 +0530

    Setting up server side

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,3 +1,22 @@
-export const getStudents = (req, res) => {
-    res.send('Router is working')
+import student from "../models/student.js"
+
+export const getStudents = async (req, res) => {
+    try {
+        // Find student of models
+        const allStudents = await student.find();
+        res.status(200).json(allStudents);
+    } catch (error) {
+        res.status(404).json({msg: error.msg});
+    }
+}
+
+export const createStudent = async (req, res) => {
+    const student = req.body;
+    const newStudent = new student(student);
+    try {
+        await newStudent.save();
+        res.status(201).json(newStudent);
+    } catch (error) {
+        res.status(409).json({msg: error.msg});
+    }
 }

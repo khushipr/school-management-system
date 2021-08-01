4fa706c7e9326c8b965b84112305fa63eb484f0c
khushipr <khushipr01@gmail.com>
2021-08-01 08:12:56 +0530

    MongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,28 @@
+// Importing modules
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+// Initializing express
+const app = express();
+
+// Setting up bodyParser for sending requests properly
+app.use(bodyParser.json({limit: '20mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
+
+// Setting up cors
+app.use(cors());
+
+// Setting up mongodb (mongoose)
+const CONNECTION_URL = "mongodb+srv://khushi:010702@cluster0.cwzaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => app.listen(PORT, () =>
+    console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((err) => console.log(err, message));
+
+mongoose.set('useFindAndModify', false);

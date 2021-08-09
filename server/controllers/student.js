2652721f2273da41b7c5fef669bc3527971d98f1
khushipr <khushipr01@gmail.com>
2021-08-09 07:22:45 +0530

    Client side setup for creating a student

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,9 +1,9 @@
-import student from "../models/student.js"
+import StudentData from "../models/student.js"
 
 export const getStudents = async (req, res) => {
     try {
         // Find student of models
-        const allStudents = await student.find();
+        const allStudents = await StudentData.find();
         res.status(200).json(allStudents);
     } catch (error) {
         res.status(404).json({msg: error.msg});
@@ -12,7 +12,7 @@ export const getStudents = async (req, res) => {
 
 export const createStudent = async (req, res) => {
     const student = req.body;
-    const newStudent = new student(student);
+    const newStudent = new StudentData(student);
     try {
         await newStudent.save();
         res.status(201).json(newStudent);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 // Importing modules
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import studentRoutes from './routes/student.js';
@@ -8,18 +7,18 @@ import studentRoutes from './routes/student.js';
 // Initializing express
 const app = express();
 
-// Using the students route
-app.use('/students', studentRoutes);
-
-// Setting up bodyParser for sending requests properly
-app.use(bodyParser.json({limit: '20mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
-
 // Setting up cors
 app.use(cors());
 
+// Setting up express for sending requests properly
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Using the students route
+app.use('/students', studentRoutes);
+
 // Setting up MongoDB (mongoose)
-const CONNECTION_URL = "mongodb+srv://khushi:010702@cluster0.cwzaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://khushiprasad:010702@cluster0.0mpkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
@@ -27,6 +26,6 @@ mongoose.connect(CONNECTION_URL, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => app.listen(PORT, () =>
     console.log(`Connection is established and running on port: ${PORT}`)
-)).catch((err) => console.log(err, message));
+)).catch((err) => console.log(err));
 
 mongoose.set('useFindAndModify', false);
